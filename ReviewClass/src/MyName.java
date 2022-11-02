69a309d3d2305239ee911283d84a113a7f0d3bd4
dainyj <117058134+dainyj@users.noreply.github.com>
2022-11-02 21:22:21 +0900

    이름

---
+++ ReviewClass/src/MyName.java
@@ -0,0 +1,10 @@
+public class MyName {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+//	s	System.out.println("이 문장이 실행될까요?.....1");
+		System.out.println("이 문장이 실행될까요?.....2");
+/*		System.out.println("이 문장이 실행될까요?.....3");
+		System.out.println("이 문장이 실행될까요?.....4");
+		*/
+		}
+}
