a96b191728699eff74f97af3aea0d5fbb32cfbb9
dainyj <117058134+dainyj@users.noreply.github.com>
2022-11-27 20:21:36 +0900

    정올 반복제어문2 q542

---
+++ JO/src/q542_rep2/Main.java
@@ -0,0 +1,12 @@
+/*10부터 20까지의 숫자를 차례대로 출력하는 프로그램을 작성하시오.for문을 사용하세요.
+*/
+package q542_rep2;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 10; i <= 20; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
