0f75fbd8380c2093dc38fef4832b26620b2c41bf
dainyj <117058134+dainyj@users.noreply.github.com>
2022-11-29 20:37:24 +0900

    반복2 q547

---
+++ JO/src/q547_rep2/Main.java
@@ -0,0 +1,13 @@
+package q547_rep2;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i=2;i<7;i++) {
+			System.out.print(i+" ");
+			for(int j=i+1; j<i+5;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
